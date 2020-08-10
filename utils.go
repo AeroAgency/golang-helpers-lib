6e7c59fc96c43765f7dd90d3383975018971b0da
aeroideaservices <53090445+aeroideaservices@users.noreply.github.com>
2020-08-10 19:06:56 +0300

    Update utils.go
    added func

--- utils.go
+++ utils.go
@@ -8,3 +8,20 @@ func StringInSlice(a string, list []string) bool {
 	}
 	return false
 }
+
+// Возвращает размер файла в байтах
+func CalcOrigBinaryLength(fileBase64String string) int {
+	l := len(fileBase64String)
+	// count how many trailing '=' there are (if any)
+	eq := 0
+	if l >= 2 {
+		if fileBase64String[l-1] == '=' {
+			eq++
+		}
+		if fileBase64String[l-2] == '=' {
+			eq++
+		}
+		l -= eq
+	}
+	return (l*3 - eq) / 4
+}
