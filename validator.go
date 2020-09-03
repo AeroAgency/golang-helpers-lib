047a3399dd5e8babcfaa0da3111abccdd1052e55
Mikhail Dadaev <dadaev@aeroidea.ru>
2020-09-03 14:27:35 +0300

    added validator rules

--- validator.go
+++ validator.go
@@ -3,17 +3,42 @@ package helpers
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/thedevsaddam/govalidator"
+	"strconv"
+	"strings"
 )
 
 type Validator struct{}
 
+// Добавление кастомного правила
+func (validator Validator) AddCustomRule(name string, fn func(field string, rule string, message string, value interface{}) error) {
+	govalidator.AddCustomRule(name, fn)
+	return
+}
+
+func (validator Validator) addUniversalMaxStringValidationRule() {
+	validator.AddCustomRule("string_len", func(field string, rule string, message string, value interface{}) error {
+		mustLen := strings.TrimPrefix(rule, "max:")
+		maxLen, err := strconv.Atoi(mustLen)
+		if err != nil {
+			panic(errors.New("govalidator: unable to parse string to integer"))
+		}
+		val := []rune(value.(string))
+		if len(val) > maxLen {
+			return fmt.Errorf("The %s field must be maximum %d char", field, maxLen)
+		}
+		return nil
+	})
+}
+
+// Валидация объекта
 func (validator Validator) ValidateProto(inputStruct interface{}, rules map[string][]string) error {
+	validator.addUniversalMaxStringValidationRule()
 	opts := govalidator.Options{
 		Data:  inputStruct,
 		Rules: rules,
 	}
-
 	v := govalidator.New(opts)
 	e := v.ValidateStruct()
 	if len(e) > 0 {
