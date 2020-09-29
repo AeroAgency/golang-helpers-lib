e6295a1f6841c053d81937dc66fbef0150981e26
ilarionov <larionov@aeroidea.ru>
2020-09-29 19:17:29 +0300

    добавил логи

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.5.0
 	github.com/thedevsaddam/govalidator v1.9.10
+	go.uber.org/zap v1.16.0
 	google.golang.org/genproto v0.0.0-20200722002428-88e341933a54
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.24.0
--- middleware.go
+++ middleware.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	uuid "github.com/satori/go.uuid"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"google.golang.org/protobuf/runtime/protoiface"
 	"net/http"
+	"strings"
 	"time"
 )
+
+// userAgent - duplicate string in code
+const userAgent = "User-Agent"
 
 type Middleware struct{}
 
@@ -66,5 +72,54 @@ func (m *Middleware) MiddlewaresHandler(h http.Handler) http.Handler {
 		h.ServeHTTP(lrw, r)
 		IncRequestCount(r, lrw)
 		SetRequestDuration(r, lrw, startTime)
+	})
+}
+
+// Logger -
+func (m *Middleware) LoggerMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		exceptions := make([]string, 0, 2)
+		exceptions = append(exceptions, "kube-probe", "Prometheus")
+		lrw := NewLoggingResponseWriter(w)
+		config := zap.NewProductionConfig()
+		config.Encoding = "json"
+		config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+		config.OutputPaths = []string{"stdout"}
+		config.EncoderConfig.TimeKey = "timestamp"
+		config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		config.EncoderConfig.CallerKey = "logger"
+		logger, _ := config.Build()
+
+		t1 := time.Now()
+		defer func() {
+			for _, v := range exceptions {
+				if !strings.HasPrefix(r.Header.Get(userAgent), v) {
+					logger.Info("",
+						zap.Any("message", map[string]interface{}{
+							"return-code": lrw.StatusCode,
+							"http-method": r.Method,
+							"request-headers": map[string]interface{}{
+								"Content-Type":   r.Header.Get("Content-Type"),
+								"Content-Length": r.Header.Get("Content-Length"),
+								userAgent:        r.Header.Get(userAgent),
+								"Server":         r.Header.Get("Server"),
+								"Via":            r.Header.Get("Via"),
+								"Accept":         r.Header.Get("Accept"),
+								"pstxid":         r.Header.Get("pstxid"),
+								"x-ps-pstxid":    r.Header.Get("x-ps-pstxid"),
+								"x-ps-sso-token": r.Header.Get("x-ps-sso-token"),
+							},
+						}),
+
+						zap.String("X-FORWARDED-FOR", r.Header.Get("X-FORWARDED-FOR")),
+						zap.String("Remote Addr", r.RemoteAddr),
+						zap.String("Proto", r.Proto),
+						zap.String("Path", r.URL.Path),
+						zap.Duration("Latency", time.Since(t1)),
+					)
+				}
+			}
+		}()
+		h.ServeHTTP(lrw, r)
 	})
 }
