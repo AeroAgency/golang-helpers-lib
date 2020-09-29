5f33b3389b40ce61ba443c8b539f929bb12355db
ilarionov <larionov@aeroidea.ru>
2020-09-29 17:40:40 +0300

    add metrics and middleware

--- metrics.go
+++ metrics.go
@@ -8,17 +8,17 @@ import (
 )
 
 type PrometheusMetrics struct {
-	requestCount    *prometheus.CounterVec
-	requestDuration *prometheus.GaugeVec
+	RequestCount    *prometheus.CounterVec
+	RequestDuration *prometheus.GaugeVec
 }
 
 func NewPrometheusMetrics() *PrometheusMetrics {
 	return &PrometheusMetrics{
-		requestCount: prometheus.NewCounterVec(prometheus.CounterOpts{
+		RequestCount: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "http_server_requests_seconds_count",
 			Help: "Application request count.",
 		}, []string{"method", "uri", "status"}),
-		requestDuration: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		RequestDuration: prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Name: "http_server_requests_seconds_sum",
 			Help: "Request duration, ms",
 		}, []string{"method", "uri", "status"}),
@@ -27,11 +27,11 @@ func NewPrometheusMetrics() *PrometheusMetrics {
 
 // IncRequestCount
 func (p *PrometheusMetrics) IncRequestCount(r *http.Request, lrw *LoggingResponseWriter) {
-	p.requestCount.WithLabelValues(r.Method, r.RequestURI, strconv.Itoa(lrw.StatusCode)).Inc()
+	p.RequestCount.WithLabelValues(r.Method, r.RequestURI, strconv.Itoa(lrw.StatusCode)).Inc()
 }
 
 // SetRequestDuration
 func (p *PrometheusMetrics) SetRequestDuration(r *http.Request, lrw *LoggingResponseWriter, startTime int) {
 	durationTime := float64((time.Now().Nanosecond() - startTime) % 1000)
-	p.requestDuration.WithLabelValues(r.Method, r.RequestURI, strconv.Itoa(lrw.StatusCode)).Set(durationTime)
+	p.RequestDuration.WithLabelValues(r.Method, r.RequestURI, strconv.Itoa(lrw.StatusCode)).Set(durationTime)
 }
