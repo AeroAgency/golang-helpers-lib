aae8d6b3ac39c7c11cb182608d17c1f54abf7ec5
aeroideaservices <53090445+aeroideaservices@users.noreply.github.com>
2020-12-23 19:19:59 +0300

    Update interceptors.go

--- interceptors.go
+++ interceptors.go
@@ -7,37 +7,52 @@ import (
 	"google.golang.org/grpc/metadata"
 	"os"
 	"time"
+	"strings"
 )
 
 func GrpcServerUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	start := time.Now()
 	md, _ := metadata.FromIncomingContext(ctx)
+	var httpRequestUserAgent string
+	var exception bool
+	exceptions := make([]string, 0, 2)
+	exceptions = append(exceptions, "kube-probe", "Prometheus")
 	var xRequestId string
 	for i, v := range md {
 		if i == "x-request-id" {
 			xRequestId = v[0]
 		}
+		if i == "grpcgateway-user-agent" {
+			httpRequestUserAgent = v[0]
+		}
+	}
+	for _, v := range exceptions {
+		if !strings.HasPrefix(httpRequestUserAgent, v) {
+			exception = true
+		}
 	}
 	logger := log.Logger{}
-	logger.SetFormatter(&log.JSONFormatter{})
-	logger.SetOutput(os.Stdout)
-	logger.Info("run grpc server")
-	logger.SetLevel(log.DebugLevel)
-	logger.WithFields(log.Fields{
-		"x-request-id": xRequestId,
-		"method":       info.FullMethod,
-		//"request":      req,
-	}).Info("Request")
+	if exception != true {
+		logger.SetFormatter(&log.JSONFormatter{})
+		logger.SetOutput(os.Stdout)
+		logger.Info("run grpc server")
+		logger.SetLevel(log.DebugLevel)
+		logger.WithFields(log.Fields{
+			"x-request-id": xRequestId,
+			"method":       info.FullMethod,
+			//"request":      req,
+		}).Info("Request")
+	}
 
 	// Calls the handler
 	h, err := handler(ctx, req)
-
-	logger.WithFields(log.Fields{
-		"x-request-id": xRequestId,
-		"method":       info.FullMethod,
-		"response":     h,
-		"time":         time.Since(start),
-	}).Info("Response")
-
+	if exception != true {
+		logger.WithFields(log.Fields{
+			"x-request-id": xRequestId,
+			"method":       info.FullMethod,
+			"response":     h,
+			"time":         time.Since(start),
+		}).Info("Response")
+	}
 	return h, err
 }
