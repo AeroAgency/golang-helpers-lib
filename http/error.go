551807fa12b6887989927de946ebc091d346c557
Mihail Dadaev <dadaev@aeroidea.ru>
2023-06-02 12:20:48 +0300

    added resp methods

---
+++ http/error.go
@@ -0,0 +1,66 @@
+package rest_handler
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type Error struct {
+	httpCode int
+	appCode  string
+	error    error
+	message  string
+}
+
+func New(s string) *Error {
+	return &Error{
+		error: errors.New(s),
+	}
+}
+
+func Errorf(s string, a ...interface{}) *Error {
+	return &Error{
+		error: fmt.Errorf(s, a...),
+	}
+}
+
+func (e *Error) SetMessage(s string, a ...interface{}) *Error {
+	e.message = fmt.Sprintf(s, a...)
+	return e
+}
+
+func (e *Error) SetAppCode(s string) *Error {
+	e.appCode = s
+	return e
+}
+
+func (e *Error) SetHttpCode(status int) *Error {
+	e.httpCode = status
+	return e
+}
+
+func (e *Error) Message() string {
+	return e.message
+}
+
+func (e *Error) AppCode() string {
+	return e.appCode
+}
+
+func (e *Error) HttpCode() int {
+	return e.httpCode
+}
+
+func (e *Error) Error() string {
+	return e.error.Error()
+}
+
+func (e *Error) MarshalJSON() ([]byte, error) {
+	m := map[string]interface{}{
+		"applicationErrorCode": e.appCode,
+		"message":              e.message,
+		"debug":                e.error,
+	}
+	return json.Marshal(m)
+}
---
+++ http/response.go
@@ -0,0 +1,56 @@
+package rest_handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// Response Ответы сервиса
+type Response struct {
+}
+
+// OK 200
+func (r Response) OK(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, data)
+}
+
+// NoContent 204
+func (r Response) NoContent(c *gin.Context) {
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func (r Response) Error(c *gin.Context, err error) {
+	if err, ok := err.(*Error); ok {
+		r.err(c, err.HttpCode(), err.AppCode(), err.Message(), err.Error())
+		return
+	}
+	r.InternalServerError(c, "APP_ERROR", err.Error(), err.Error())
+}
+
+// InternalServerError 500
+func (r Response) InternalServerError(c *gin.Context, applicationErrorCode, message, debug string) {
+	r.err(c, http.StatusInternalServerError, applicationErrorCode, message, debug)
+}
+
+// BadRequestError 400
+func (r Response) BadRequestError(c *gin.Context, debug string) {
+	r.err(c, http.StatusBadRequest, "BAD_REQUEST", "bad request", debug)
+}
+
+// NotFoundError 404
+func (r Response) NotFoundError(c *gin.Context, debug string) {
+	r.err(c, http.StatusNotFound, "NOT_FOUND", "not found", debug)
+}
+
+// ForbiddenError 403
+func (r Response) ForbiddenError(c *gin.Context, debug string) {
+	r.err(c, http.StatusForbidden, "FORBIDDEN", "bad request", debug)
+}
+
+func (r Response) err(c *gin.Context, httpErrorCode int, applicationErrorCode, message, debug string) {
+	c.JSON(httpErrorCode, gin.H{
+		"applicationErrorCode": applicationErrorCode,
+		"message":              message,
+		"debug":                debug,
+	})
+}
