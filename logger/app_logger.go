571b40436de311b54399cea7945238e3e9e66958
Mihail Dadaev <dadaev@aeroidea.ru>
2023-06-03 20:26:03 +0300

    fixed logger

--- logger/app_logger.go
+++ logger/app_logger.go
@@ -8,21 +8,21 @@ type AppLogger struct {
 	logger logr.Logger
 }
 
-func (al AppLogger) Init(l logr.Logger) {
+func (al *AppLogger) Init(l logr.Logger) {
 	al.logger = l
 }
 
 // Debug Сообщение отладочного уровня
-func (al AppLogger) Debug(msg string, keysAndValues ...interface{}) {
+func (al *AppLogger) Debug(msg string, keysAndValues ...interface{}) {
 	al.logger.V(1).Info(msg, keysAndValues)
 }
 
 // Info Сообщение информационного уровня
-func (al AppLogger) Info(msg string, keysAndValues ...interface{}) {
+func (al *AppLogger) Info(msg string, keysAndValues ...interface{}) {
 	al.logger.Info(msg, keysAndValues)
 }
 
 // Error Сообщение об ошибке
-func (al AppLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+func (al *AppLogger) Error(err error, msg string, keysAndValues ...interface{}) {
 	al.logger.Error(err, msg, keysAndValues)
 }
