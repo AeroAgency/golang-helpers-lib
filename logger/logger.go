1c906f705f6e9e3fcae311986ab1683353246b1f
mihaildadaev <dadaev@aeroidea.ru>
2023-08-02 16:36:18 +0300

    changed logger

--- logger/logger.go
+++ logger/logger.go
@@ -10,18 +10,9 @@ import (
 
 // duplicate strings in code
 const (
-	contentType       = "Content-Type"
-	contentLength     = "Content-Length"
-	userAgent         = "User-Agent"
-	server            = "Server"
-	via               = "Via"
-	accept            = "Accept"
-	pstxid            = "pstxid"
-	xPsPstxid         = "x-ps-pstxid"
-	xPsSsoToken       = "x-ps-sso-token"
-	frontEndRequestID = "FrontEnd-Request-ID"
-	xForwardedFor     = "X-FORWARDED-FOR"
-	employeeNumber    = "employee-number"
+	contentType = "Content-Type"
+	userAgent   = "User-Agent"
+	accept      = "Accept"
 )
 
 // exceptions - User-Agent exceptions
@@ -42,7 +33,6 @@ func Logger() gin.HandlerFunc {
 					exception = true
 				}
 			}
-
 			if !exception {
 				requestParams := zerolog.Dict()
 				for k, v := range c.Request.URL.Query() {
@@ -54,29 +44,16 @@ func Logger() gin.HandlerFunc {
 					Str("http-method", c.Request.Method).
 					Dict("request-headers", zerolog.Dict().
 						Str(contentType, c.Request.Header.Get(contentType)).
-						Str(contentLength, c.Request.Header.Get(contentLength)).
 						Str(userAgent, c.Request.Header.Get(userAgent)).
-						Str(server, c.Request.Header.Get(server)).
-						Str(via, c.Request.Header.Get(via)).
-						Str(accept, c.Request.Header.Get(accept)).
-						Str(pstxid, c.Request.Header.Get(pstxid)).
-						Str(xPsPstxid, c.Request.Header.Get(xPsPstxid)).
-						Str(xPsSsoToken, c.Request.Header.Get(xPsSsoToken)).
-						Str(frontEndRequestID, c.Request.Header.Get(frontEndRequestID)),
+						Str(accept, c.Request.Header.Get(accept)),
 					).
-					Str(xForwardedFor, c.Request.Header.Get(xForwardedFor)).
-					Str("Remote Addr", c.Request.RemoteAddr).
-					Str("Proto", c.Request.Proto).
 					Str("Path", c.Request.URL.Path).
 					Dur("Latency", time.Since(t1)).
-					Str(employeeNumber, c.Request.Header.Get(employeeNumber)).
 					Dict("request-params", requestParams),
 				).Msg("")
 			}
-
 			exception = false
 		}()
-
 		c.Next()
 	}
 }
