2c13fd3e2306660199c99bd31b9f547680966974
mihaildadaev <dadaev@aeroidea.ru>
2023-08-08 10:44:54 +0300

    added new type

---
+++ type/stringint.go
@@ -0,0 +1,31 @@
+package _type
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type StringInt int
+
+func (st *StringInt) UnmarshalJSON(b []byte) error {
+	var item interface{}
+	if err := json.Unmarshal(b, &item); err != nil {
+		return err
+	}
+
+	switch v := item.(type) {
+	case int:
+		*st = StringInt(v)
+	case float64:
+		*st = StringInt(int(v))
+	case string:
+		i, err := strconv.Atoi(v)
+		if err != nil {
+			return err
+
+		}
+		*st = StringInt(i)
+	}
+
+	return nil
+}
